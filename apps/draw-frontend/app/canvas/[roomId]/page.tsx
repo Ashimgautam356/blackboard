e0ad1fac4fd352d9de828c7bcf2bf313af8d237d
Ashim Gautam <computer77356@gmail.com>
2025-02-27 08:40:11 +0545

    restructure

--- apps/draw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/draw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { initDraw } from "@/draw";
 import { useEffect, useRef } from "react"
 
 
@@ -7,39 +8,7 @@ export default function Canvas(){
     useEffect(()=>{
 
         if(canvasRef.current){
-            const canvas = canvasRef.current
-            const ctx = canvas.getContext("2d");
-
-            if(!ctx){
-                return
-            }
-            // ctx.strokeRect(25,25,100,100);
-
-            let clicked = false; 
-            let startX = 0; 
-            let startY = 0; 
-
-            canvas.addEventListener("mousedown",(e)=>{
-                clicked = true;
-                
-                startX= e.clientX
-                startY =e.clientY 
-            })
-            canvas.addEventListener("mouseup",(e)=>{
-                clicked = false;
-
-            })
-            canvas.addEventListener("mousemove",(e)=>{
-                if(clicked){
-                    const width = e.clientX - startX; 
-                    const height = e.clientY - startY; 
-
-                    ctx.clearRect(0,0,canvas.width, canvas.height);
-                    ctx.strokeRect(startX,startY,width,height)
-
-                }
-            })
-
+            initDraw(canvasRef.current)
 
         }
 
@@ -47,7 +16,7 @@ export default function Canvas(){
     return(
         <div>
 
-            <canvas ref={canvasRef} width={500} height={500}></canvas>
+            <canvas ref={canvasRef} width={800} height={800}></canvas>
 
         </div>
     )
